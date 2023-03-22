1bb135d1ac75c0db0ea9d6cfea4fb786c3316965
imanuel.suharli <imanuel.suharli@tokopedia.com>
2023-03-23 00:10:53 +0700

    test github action

--- app.go
+++ app.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,7 +11,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": time.Now().String(),
 		})
 	})
 	r.Run() /// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
